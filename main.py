1104c41f858a65d47818760212a96df183c20acb
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2025-04-17 11:31:28 -0300

    refactor: set debug with false

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def get_emails():
     return {"data": emails}, 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
